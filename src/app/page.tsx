e78179dd29ff5b6ea639c38b6573185b0cebb9d7
AndersonOls <anderson.silva1996@hotmail.com>
2023-07-13 15:26:29 -0300

    criação playlist e buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import {Home as HomeIcon, Search, Library} from 'lucide-react'
+import {Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight} from 'lucide-react'
 
 export default function Home() {
   return (
@@ -16,9 +16,24 @@ export default function Home() {
               <Library/>
               Your Library</a>
           </nav>
+         <nav className='mt-10 pt-10 border-t border-zinc-800 flex flex-col gap-3'>
+          <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist</a>
+          <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist</a>
+          <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist</a>
+          <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist</a>
+          <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist</a>
+       
+         </nav>
         </aside>
-        <main className="flex-1">
-          main
+        <main className="flex-1 p-6">
+          <div className="flex-1 items-center gap-4">
+            <button className='p-1 rounded-full bg-black/40 '>
+              <ChevronLeft/>
+            </button>
+            <button className='p-1 rounded-full bg-black/40 '>
+              <ChevronRight/>
+            </button>
+          </div>
         </main>
       </div>
       <div>
